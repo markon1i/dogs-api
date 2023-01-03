923d759206cc449173b95a88ef604c482ca0cbf2
markon1i <markoschneider34@gmail.com>
2023-01-03 11:11:53 -0300

    asd

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
